ae65790116f9ad2a20d738f5542cdf51c234af14
Vkosa <32805999+Vkosa@users.noreply.github.com>
2021-03-04 04:02:01 -0500

    assignment4

---
+++ large-scale-programming/src/org/howard/edu/lsp/assignment4/Integerset.java
@@ -0,0 +1,178 @@
+package org.howard.edu.lsp.assignment4;
+
+import java.util.*;
+
+/** this contains the Integerset class 
+ * the class throws the Integerset and extends the exception class
+ */
+public class Integerset {
+	private ArrayList<Integer> set= new ArrayList<>();
+	  
+	   public Integerset(ArrayList list) {
+	       this.set=list;
+	   }
+	  
+	  
+	  
+	//   clears the all representation of class
+	   public void clear() {
+	       set.clear();
+	   }
+	  
+	//   returns the length of set
+	  
+	   public int length() {
+	       return set.size();
+	   }
+	  
+	//   return true if the 2 sets  are equal. false otherwise
+	   public boolean equals(Integerset b) {
+	      
+	       ArrayList<Integer> list2=b.set;
+	         
+	       if(set.size()!=list2.size()) {
+	           return false;
+	       }
+	         
+	       ArrayList<Integer> temp1=set;
+	         
+	       ArrayList<Integer> temp2=list2;
+	         
+	       Collections.sort(temp1);
+	       Collections.sort(temp2);
+	         
+	       for(int i=0;i<temp1.size();i++) {
+	           if(temp1.get(i)!=temp2.get(i)) {
+	               return false;
+	           }
+	       }
+	         
+	       return true;
+	      
+	   }
+	   
+	   class Listemptyexception extends Exception {
+
+	   }
+	  
+	//   returns the largest number in the list and throws exception if the list is empty
+	  
+	   public int largest() throws Listemptyexception {
+	      
+	       if(set.size()==0) {
+	           Listemptyexception e= new Listemptyexception();
+	           throw e;
+	       }
+	      
+	       int max=0;
+	      
+	       for(int i=0;i<set.size();i++) {
+	           if(set.get(i)>max) {
+	               max=set.get(i);
+	           }
+	       }
+	      
+	       return max;
+	      
+	   }
+	  
+	  //finds minimum value
+	   public int smallest() throws Listemptyexception {
+	      
+	       if(set.size()==0) {
+	           Listemptyexception e= new Listemptyexception();
+	           throw e;
+	       }
+	      
+	       int min=Integer.MAX_VALUE;
+	      
+	       for(int i=0;i<set.size();i++) {
+	           if(set.get(i)<min) {
+	               min=set.get(i);
+	           }
+	       }
+	      
+	       return min;
+	      
+	   }
+	  
+
+	  //adds item to list
+	  
+	   public void add(int item ) {
+	      
+	       boolean exist=false;
+	      
+	       for(int i=0;i<set.size();i++) {
+	          
+	           if(set.get(i)==item) {
+	               exist=true;
+	           }
+	       }
+	      
+	       if(exist==false) {
+	           set.add(item);
+	       }
+	      
+	   }
+	  
+	//   removes item if it already exists in the list
+	  
+	   public void remove(int item ) {
+	      
+	      
+	      
+	       for(int i=0;i<set.size();i++) {
+	          
+	           if(set.get(i)==item) {
+	               set.remove(i);
+	           }
+	       }
+	      
+	   }
+	  
+	   public Integerset union(Integerset list2) {
+	      
+	       ArrayList<Integer> temp=list2.getlist();
+	      
+	       Set<Integer> set = new HashSet<>();
+	       set.addAll(set);
+	       set.addAll(temp);
+	      
+	       ArrayList<Integer> list3= new ArrayList<>(set);
+	      
+	       Integerset list4= new Integerset(list3);
+	      
+	       return list4;
+	      
+	   }
+	  //finds item in both lists that equal each other
+	   public Integerset intersect(Integerset list2) {
+	      
+	       ArrayList<Integer> temp=list2.getlist();
+	      
+	       set.retainAll(temp);
+	      
+	       Integerset list4= new Integerset(set);
+	      
+	       return list4;
+	   }
+	  
+	   public Integerset difference (Integerset list2) {
+	      
+	       ArrayList<Integer> temp=list2.getlist();
+	      
+	       set.removeAll(temp);
+	      
+	       Integerset list4= new Integerset(set);
+	      
+	       return list4;
+	      
+	   }
+	  
+	   public ArrayList<Integer> getlist(){
+	       return set;
+	   }
+
+
+}
---
+++ large-scale-programming/src/org/howard/edu/lsp/assignment4/main.java
@@ -0,0 +1,63 @@
+package org.howard.edu.lsp.assignment4;
+
+import java.util.ArrayList;
+
+import org.howard.edu.lsp.assignment4.Integerset.Listemptyexception;
+/** contains implementation class
+ * Each set can be found here as well
+ */
+
+public class main {
+
+	public static void main(String[] args) throws Listemptyexception {
+		// TODO Auto-generated method stub
+		  
+		ArrayList<Integer> list1= new ArrayList<>();
+  
+		Integerset set1= new Integerset(list1);
+		
+		ArrayList<Integer> list2= new ArrayList<>();
+
+		Integerset set2= new Integerset(list2);
+		
+
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+		set1.add(4);
+		set1.add(5);
+		set1.add(9);
+		set1.add(6);
+		
+		
+		set2.add(1);
+		set2.add(40);
+		set2.add(12);
+		set2.add(33);
+		set2.add(53);
+		set2.add(66);
+		
+		  
+		System.out.println("Set 1: " + set1.getlist());
+		System.out.println("Set 2: " + set2.getlist());
+		  
+		System.out.println("Is equal: "+set1.equals(set2));
+		  
+		System.out.println("Largest element in set1: " + set1.largest());
+		  
+		System.out.println("Smallest element in set2: "+ set2.smallest());
+		  
+		Integerset newset= set1.union(set2);
+		  
+		System.out.println("Union: " + newset.getlist() );
+		  
+		Integerset setj= set1.intersect(set2);
+		  
+		System.out.println("intersection: " + setj.getlist() );
+		  
+
+		      
+		   }
+
+
+}
